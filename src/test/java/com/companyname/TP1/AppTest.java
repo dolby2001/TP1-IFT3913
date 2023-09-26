cf02dd37126d119b0971b8b4c60078fa158882f4
Dolby <olipod89@gmail.com>
2023-09-26 15:32:41 -0400

    Partie 1 TLOC done

--- src/test/java/com/companyname/TP1/AppTest.java
+++ src/test/java/com/companyname/TP1/AppTest.java
@@ -1,38 +1,64 @@
 package com.companyname.TP1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    public static int tloc(String filePath) throws IOException {
+        int loc = 0;
+        boolean inMultiLineComment = false;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("//")) {
+                    continue;
+                }
+
+                if (line.startsWith("/*")) {
+                    inMultiLineComment = true;
+                }
+
+                if (!inMultiLineComment && !line.startsWith("*/")) {
+                    loc++;
+                }
+
+                if (line.endsWith("*/")) {
+                    inMultiLineComment = false;
+                }
+            }
+        }
+
+        return loc;
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static void testTloc(String filePath) {
+        // Replace with the actual path to your Java source file
+
+        try {
+            int expectedLOC = 8; // Replace with the expected LOC count
+            int actualLOC = tloc(filePath);
+            assertEquals(expectedLOC, actualLOC);
+            System.out.println("TLOC : " + actualLOC);
+        } catch (Exception e) {
+            fail("An exception occurred: " + e.getMessage());
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public static void main(String[] args) {
+
+        // Example usage
+        String filePath = "src/main/java/com/companyname/TP1/App.java";
+
+        testTloc(filePath);
     }
+
+
 }
